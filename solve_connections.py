e719554a35890edd74a7e990f3a6b608fa1b0104
Zachary Sparrow <zachsparrow95@gmail.com>
2025-04-13 16:03:14 -0400

    defn transformer

--- solve_connections.py
+++ solve_connections.py
@@ -1,10 +1,12 @@
-#from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 #from sklearn.metrics.pairwise import linear_kernel
 #from sklearn.metrics.pairwise import rbf_kernel
 import csv
 import matplotlib.pyplot as plt
 #import fasttext.util
 from nltk.corpus import wordnet as wn
+from sentence_transformers import SentenceTransformer
 
 def read_csv(file_path):
     data_array = []
@@ -33,8 +35,20 @@ print(puzzle_words)
 #sim_mat = cosine_similarity(vecs)
 #sim_mat = linear_kernel(vecs)
 #sim_mat = rbf_kernel(vecs, gamma=1/10)
+model = SentenceTransformer("all-MiniLM-L6-v2")
+#model = SentenceTransformer("all-mpnet-base-v2")
 
 synsets = [wn.synsets(w) for w  in puzzle_words]
+definitions = [[w.definition() for w in s] for s in synsets]
+
+print()
+for s in synsets:
+    print(s)
+
+print()
+for d in definitions:
+    print(d)
+
 sim_mat = [[0 for j in range(16)] for i in range(16)]
 for wi,w in enumerate(synsets):
     for xi,x in enumerate(synsets):
@@ -44,8 +58,21 @@ for wi,w in enumerate(synsets):
                 all_sims.append(wn.wup_similarity(ws,xs))
         sim_mat[wi][xi] = max(all_sims)
 
+sim_mat2 = [[0 for j in range(16)] for i in range(16)]
+embeddings = [model.encode(defn) for defn in definitions]
+for wi,w in enumerate(embeddings):
+    for xi,x in enumerate(embeddings):
+        all_sims = []
+        for ws in w:
+            for xs in x:
+                all_sims.append(cosine_similarity([ws],[xs])[0][0])
+        sim_mat2[wi][xi] = max(np.abs(all_sims))
 
 plt.matshow(sim_mat)
 plt.colorbar()
-plt.title("Word Similarities")
+plt.title("Word Similarities synsets")
+plt.show()
+plt.matshow(sim_mat2)
+plt.colorbar()
+plt.title("Word Similarities defn Transformer")
 plt.show()
