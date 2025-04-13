a973d933b005ef463fabfba5cb5dadd258a9ef04
Zachary Sparrow <zachsparrow95@gmail.com>
2025-04-13 10:19:07 -0400

    wordnet

--- solve_connections.py
+++ solve_connections.py
@@ -1,7 +1,10 @@
-from sklearn.metrics.pairwise import cosine_similarity
+#from sklearn.metrics.pairwise import cosine_similarity
+#from sklearn.metrics.pairwise import linear_kernel
+#from sklearn.metrics.pairwise import rbf_kernel
 import csv
 import matplotlib.pyplot as plt
-import fasttext.util
+#import fasttext.util
+from nltk.corpus import wordnet as wn
 
 def read_csv(file_path):
     data_array = []
@@ -18,16 +21,29 @@ def read_csv(file_path):
          return []
     return data_array
 
-puzzle = read_csv("puzzles/test_puzzle_1.txt")
+puzzle = read_csv("puzzles/test_puzzle_3.txt")
 puzzle_words = [x.lower().strip() for xs in puzzle for x in xs]
 print(puzzle_words)
 
-fasttext.util.download_model('en', if_exists='ignore')  
-ft = fasttext.load_model('cc.en.300.bin')
+#fasttext.util.download_model('en', if_exists='ignore')  
+#ft = fasttext.load_model('cc.en.300.bin')
+#
+#vecs = [ft.get_word_vector(w) for w in puzzle_words]
 
-vecs = [ft.get_word_vector(w) for w in puzzle_words]
+#sim_mat = cosine_similarity(vecs)
+#sim_mat = linear_kernel(vecs)
+#sim_mat = rbf_kernel(vecs, gamma=1/10)
 
-sim_mat = cosine_similarity(vecs)
+synsets = [wn.synsets(w) for w  in puzzle_words]
+sim_mat = [[0 for j in range(16)] for i in range(16)]
+for wi,w in enumerate(synsets):
+    for xi,x in enumerate(synsets):
+        all_sims = []
+        for ws in w:
+            for xs in x:
+                all_sims.append(wn.wup_similarity(ws,xs))
+        sim_mat[wi][xi] = max(all_sims)
+
 
 plt.matshow(sim_mat)
 plt.colorbar()
